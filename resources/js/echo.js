402101495b21126b3a1be544335b90e731eed5bd
indeloper <indeloper@gmail.com>
2025-12-18 20:15:26 +0300

    first

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -3,12 +3,14 @@ import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
 window.Pusher = Pusher;
 
+const isProduction = window.location.protocol === 'https:';
+
 window.Echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
+    wsHost: import.meta.env.VITE_REVERB_HOST ?? window.location.hostname,
+    wsPort: import.meta.env.VITE_REVERB_PORT ?? (isProduction ? 443 : 8080),
     wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+    forceTLS: isProduction,
     enabledTransports: ['ws', 'wss'],
 });
